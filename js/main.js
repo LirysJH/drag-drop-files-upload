9c28fc35a2f97017d0d929bffce676a0b6564724
LirysJH <48212972+LirysJH@users.noreply.github.com>
2020-07-07 23:52:21 +0300

    Added thumbnails and styling

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 let dropArea = document.querySelector('.container'); // general drag & drop area
+let progressBar = document.querySelector('.progress-bar'); // progress bar
 
 ;['dragenter', 'dragover', 'dragleave', 'drop'].forEach(event => {
     dropArea.addEventListener(event, (event) => {
@@ -8,14 +9,50 @@ let dropArea = document.querySelector('.container'); // general drag & drop area
     }, false)
 });
 
+// change style when drag & drop
 ;['dragenter', 'dragover'].forEach(eventName => {
     dropArea.addEventListener(eventName, (e) => {
         dropArea.classList.add('drag-drop');
-    }, false)
+    }, false);
+    progressBar.addEventListener(eventName, (e) => {
+        progressBar.classList.add('active');
+    }, false);
 });
 
 ;['dragleave', 'drop'].forEach(eventName => {
     dropArea.addEventListener(eventName, (e) => {
         dropArea.classList.remove('drag-drop');
-    }, false)
+    }, false);
+    progressBar.addEventListener(eventName, (e) => {
+        progressBar.classList.remove('active');
+    }, false);
 });
+
+// get files when dropped
+dropArea.addEventListener('drop', (e) => {
+    let dt = e.dataTransfer;
+    let files = dt.files;
+
+    getFiles(files);
+}, false);
+
+getFiles = (files) => {
+    files = [...files];
+    files.forEach(uploadFiles);
+    files.forEach(previewFiles);
+};
+
+uploadFiles = (file) => {
+    console.log("Upploading files");
+};
+
+previewFiles = (file) => {
+    let reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = () => {
+      let img = document.createElement('img');
+      img.classList.add("icon");
+      img.src = reader.result;
+      document.querySelector('.thumbnails').appendChild(img);
+    }
+};
